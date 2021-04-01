3c16a8db8a6c1305062e4b6c08af59cee33f5d2f
Samuel <charlieaussie98@gmail.com>
2021-04-01 13:50:00 +0200

    Initial Commit LoginFunction

--- src/main/java/stepDefinitions/LoginFunction.java
+++ src/main/java/stepDefinitions/LoginFunction.java
@@ -1,4 +1,45 @@
 package stepDefinitions;
 
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
 public class LoginFunction {
+    @Given("^User is already on the login webpage\\.$")
+    public void user_is_already_on_the_login_webpage() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^title of the webpage is asserted\\.$")
+    public void title_of_the_webpage_is_asserted() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^User enter no information on the email box\\.$")
+    public void user_enter_no_information_on_the_email_box() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^User enter account name with more then one hundred \"([^\"]*)\"\\.$")
+    public void user_enter_account_name_with_more_then_one_hundred(String arg1) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^User enter an already busy \"([^\"]*)\"\\.$")
+    public void user_enter_an_already_busy(String arg1) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^User creates an account with no problems\\.$")
+    public void user_creates_an_account_with_no_problems() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
 }
