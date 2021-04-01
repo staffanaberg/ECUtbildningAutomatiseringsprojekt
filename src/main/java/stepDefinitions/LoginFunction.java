e942db5e1f0a232738a3ed1c564384628f940fb4
Samuel <charlieaussie98@gmail.com>
2021-04-01 14:54:48 +0200

    Initial Commit 1.1

--- src/main/java/stepDefinitions/LoginFunction.java
+++ src/main/java/stepDefinitions/LoginFunction.java
@@ -1,45 +1,82 @@
 package stepDefinitions;
 
-import cucumber.api.PendingException;
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 
 public class LoginFunction {
-    @Given("^User is already on the login webpage\\.$")
-    public void user_is_already_on_the_login_webpage() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    WebDriver driver;
+
+    @Before
+    public void init() {
+        System.out.println("Starting the Program");
     }
 
-    @When("^title of the webpage is asserted\\.$")
-    public void title_of_the_webpage_is_asserted() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    @After
+    public void exit() {
+        System.out.println("Exiting the Program");
+
     }
+
+
+    @Given("^User is already on the login webpage\\.$")
+    public void user_is_already_on_the_login_webpage() throws InterruptedException {
+        System.setProperty("webdriver,chrome,driver", "chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get("https://login.mailchimp.com/signup/");
+        driver.manage().window().maximize();
+        Thread.sleep(1000);
+
+
+    }
+
+    @When("^title of the webpage is asserted and User accept cookies$")
+    public void title_of_the_webpage_is_asserted_and_User_accept_cookies() {
+
+        driver.findElement(By.id("onetrust-accept-btn-handler")).click();
+        String title = driver.getTitle();
+        System.out.println(title);
+        Assert.assertEquals("Signup | Mailchimp", title);
+    }
+
 
     @Then("^User enter no information on the email box\\.$")
-    public void user_enter_no_information_on_the_email_box() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    public void user_enter_no_information_on_the_email_box() throws InterruptedException {
+        Thread.sleep(1500);
+
+        driver.findElement(By.id("new_username")).sendKeys("Shagtanagotha");
+        driver.findElement(By.id("new_password")).sendKeys("!Nyarlahotep98");
+        Thread.sleep(1000);
+        driver.findElement(By.id("create-account")).click();
+        String email = "";
+        System.out.println(email);
+        Thread.sleep(1000);
+        driver.findElement(By.id("email")).sendKeys(email);
+        Assert.assertEquals("", email);
+
+
     }
 
-    @Then("^User enter account name with more then one hundred \"([^\"]*)\"\\.$")
-    public void user_enter_account_name_with_more_then_one_hundred(String arg1) throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    @Then("^User enter account name with more then one hundred 'characters'\\.$")
+    public void user_enter_account_name_with_more_then_one_hundred_characters(String characters) {
+
     }
 
-    @Then("^User enter an already busy \"([^\"]*)\"\\.$")
-    public void user_enter_an_already_busy(String arg1) throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    @Then("^User enter an already busy 'name'\\.$")
+    public void user_enter_an_already_busy_name(String name) {
+
     }
 
     @Then("^User creates an account with no problems\\.$")
-    public void user_creates_an_account_with_no_problems() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    public void user_creates_an_account_with_no_problems() {
+
     }
+
 
 }
--- src/test/resources/Features/mailchimp.feature
+++ src/test/resources/Features/mailchimp.feature
@@ -3,9 +3,9 @@ Feature: In the login function I want to create an account and register as a use
 
   Scenario: I enter information in login page to enter Mailchimp website.
     Given User is already on the login webpage.
-    When title of the webpage is asserted.
+    When title of the webpage is asserted and User accept cookies
     Then User enter no information on the email box.
-    And User enter account name with more then one hundred "characters".
-    Then User enter an already busy "name".
+    And User enter account name with more then one hundred 'characters'.
+    Then User enter an already busy 'name'.
     Then User creates an account with no problems.
 
